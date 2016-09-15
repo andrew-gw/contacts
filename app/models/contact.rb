0ac01504868abbdbc82bc4bb58602359abc102d1
Andrew Goodrick-Werner <andrew@Andrews-MacBook-Pro.local>
2016-09-15 10:46:17 -0400

    Adds buttons and glyphicons

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,13 @@
 class Contact < ActiveRecord::Base
   extend Dragonfly::Model
   include Avatarable
+
   dragonfly_accessor :photo
+
   validates :fname, :lname, presence: true
+
   belongs_to :user
+
   def avatar_text
     fname.chr
   end

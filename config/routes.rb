cdf83dedaaf9407818f8afe892c04598b4489a99
Andrew Goodrick-Werner <andrew@Andrews-MacBook-Pro.local>
2016-09-15 16:05:22 -0400

    Adds auth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'login',  to: 'sessions#new', as: :login
+  get 'logout', to: 'sessions#destroy', as: :logout
+  resources :sessions, only: [:create]
+  
   resources :contacts
 
   # Avatar routes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915182047) do
+ActiveRecord::Schema.define(version: 20160915193813) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "fname"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160915182047) do
     t.datetime "updated_at", null: false
     t.string   "photo_uid"
     t.string   "photo_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

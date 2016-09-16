c9e7343941782bad4452757d4d635ed67ea5552d
Andrew Goodrick-Werner <andrew@Andrews-MacBook-Pro.local>
2016-09-16 13:46:12 -0400

    Implements basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def authenticate_user!
-    if current_user.nil?
-      redirect_to login_url
-    end
-  end
-
-  private
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :current_user
+  include SessionsHelper
 end

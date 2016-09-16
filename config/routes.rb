8c3b853c749723b4f3e6538fe88dc5af2b2d33bc
Andrew Goodrick-Werner <andrew@Andrews-MacBook-Pro.local>
2016-09-16 11:46:23 -0400

    Adds user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get  '/signup',  to: 'users#new'
+
   resources :contacts
 
   # Avatar routes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915193813) do
+ActiveRecord::Schema.define(version: 20160916153209) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "fname"
@@ -24,10 +24,12 @@ ActiveRecord::Schema.define(version: 20160915193813) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

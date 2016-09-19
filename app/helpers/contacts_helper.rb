b1a4a4a41d863b01b3b51cda4999ace7364abb69
Andrew Goodrick-Werner <andrew.gw@me.com>
2016-09-18 23:10:09 -0400

    View changes

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -3,7 +3,7 @@ module ContactsHelper
 		if contact.photo.nil?
 			image_tag contact.avatar_url, options
 		else
-			image_tag contact.photo.thumb('256x256#').url, options
+			image_tag contact.photo.thumb('192x192#').url, options
 		end
 	end
 end
--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -16,7 +16,7 @@ module Avatarable
   end
 
   def avatar_size
-    256
+    192
   end
 
   def avatar_param
